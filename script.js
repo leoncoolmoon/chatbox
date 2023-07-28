5ff003bd261075fe6a035edef008303b40a8afae
Jack <leoncoolmoon@hotmail.com>
2023-07-28 14:48:45 +1000

    Update script.js

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function chat(message) {
 
   const data = {
     //"model": "gpt-3.5-turbo-16k",
-    "model": "gpt-4-32k-0613",
+    "model": "gpt-4-0613",
     "messages": message,
     "temperature": 0.7
   };
